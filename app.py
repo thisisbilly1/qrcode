3ef18773a7a186ab03f503a361dfa5ca56530ba8
nicholaskoerber <nicholas.koerber@siowi.com>
2020-11-30 15:46:10 -0600

    test4

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask, request
-import pyqrcode
+# coding=utf-8
+from flask import Flask, request, send_file
+
+from flask_qrcode import QRcode
+
+
 app = Flask(__name__)
+qrcode = QRcode(app)
 
 
 @app.route('/')
-def home():
-	link = request.args.get('link')
-	url = pyqrcode.create(link)
-	return url
+def index():
+    return "hello world lol"
 
-#app.run(debug=False)
+
+@app.route('/qrcode', methods=['GET'])
+def get_qrcode():
+    # please get /qrcode?data=<qrcode_data>
+    data = request.args.get('data', '')
+    return send_file(
+        qrcode(data, mode='raw'),
+        mimetype='image/png'
+    )
+
+'''
+if __name__ == '__main__':
+    app.run(debug=False)
+'''
