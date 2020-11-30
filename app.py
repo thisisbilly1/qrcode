f15edf9319defc75d11b94b223e8ba90c0b96d0b
nicholaskoerber <nicholas.koerber@siowi.com>
2020-11-30 16:22:58 -0600

    header fixes

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def index():
 @app.route('/qrcode', methods=['GET'])
 def get_qrcode():
 	# please get /qrcode?data=<qrcode_data>
-	data = request.args.get('data', '')
+	#data = request.args.get('data', '')
+	data ="https://apps.powerapps.com/play/2ef38032-cf7b-496f-90ca-4cb0d73c8edf?tenantId=a41c82de-1ce1-4077-8cee-685e4285be5c&source=portal"
 	deviceid = request.args.get('deviceid', '')
+	data+="&deviceid="+str(deviceid)
 	
 	return render_template('template.html', data=data, deviceid=deviceid)
 
